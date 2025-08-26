464ea0b7138f6705e51ad618911f904236c79fbb
Fer-2202 <imarin.marin2003fwd@gmail.com>
2025-08-26 15:11:17 -0600

    Correciones al servidor

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Middleware opcional para parsear JSON
-app.use(express.json());
+// Middleware para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta raíz
+// Ruta raíz que envía index.html
 app.get('/', (req, res) => {
-    res.send('Servidor educativo activo en el puerto 3000');
+    res.sendFile(path.join(__dirname, 'public', 'pages/index.html'));
 });
 
 // Iniciar servidor
