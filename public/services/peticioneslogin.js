fd724f70aaeaf6a83de84f300db4d75f6017f2b8
Fer-2202 <imarin.marin2003fwd@gmail.com>
2025-08-26 16:00:07 -0600

    Se crearon los usuarios y se hizo la solicitud get en la carpeta /public/services

---
+++ public/services/peticioneslogin.js
@@ -0,0 +1,17 @@
+//Esta es la función que lee las películas
+async function getUser() {
+    try {
+        const response = await fetch('http://localhost:3001/usuarios', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const usuarios = await response.json()
+        return usuarios
+
+    } catch (error) {
+        console.log("Ocurrió un error al obtener los usuarios" + error)
+    }
+}
